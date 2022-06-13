f2295f0741815350710c0d38586eddc08b4666a6
resab <resab@qq.com>
2022-06-13 23:31:36 +0800

    designModel

--- com/resab/juc01/交替/交替打印/Demo.java
+++ com/resab/juc01/交替/交替打印/Demo.java
@@ -5,24 +5,36 @@ public class Demo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        FooBar foo = new FooBar(33);
-        Runnable runnable = new Runnable
-                () {
-            @Override
-            public void run() {
-                System.out.println("foo");
-            }
-        };
-        Runnable barRunnble = new Runnable
-                () {
-            @Override
-            public void run() {
-                System.out.println("bar");
-            }
-        };
-        foo.bar(barRunnble);
-        foo.foo(runnable);
+        FooBar foo = new FooBar(22);
 
+        new Thread(() -> {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("bar");
+                }
+            };
+            try {
+                foo.bar(runnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("foo");
+                }
+            };
+            try {
+                foo.foo(runnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
     }
 }
--- com/resab/juc01/交替/交替打印/FooBar.java
+++ com/resab/juc01/交替/交替打印/FooBar.java
@@ -3,10 +3,13 @@ package com.resab.juc01.交替.交替打印;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+
+//手太阴肺经 BLOCKING Queue
 public class FooBar {
     private int n;
-    private BlockingQueue<Integer> bar = new LinkedBlockingQueue<>(0);
-    private BlockingQueue<Integer> foo = new LinkedBlockingQueue<>(0);
+
+    private BlockingQueue<Integer> bar = new LinkedBlockingQueue<>(1);
+    private BlockingQueue<Integer> foo = new LinkedBlockingQueue<>(1);
 
     public FooBar(int n) {
         this.n = n;
@@ -14,22 +17,18 @@ public class FooBar {
 
     public void foo(Runnable printFoo) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-            foo.put(i);
-            printFoo.run();
             bar.put(i);
+            foo.take();
+            printFoo.run();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         for (int i = 0; i < n; i++) {
+            foo.put(i);
             bar.take();
             printBar.run();
-            foo.take();
+
         }
     }
 }
-
-//作者：idasmilence
-//        链接：https://leetcode.cn/problems/print-foobar-alternately/solution/duo-xian-cheng-liu-mai-shen-jian-ni-xue-d220n/
-//        来源：力扣（LeetCode）
-//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
