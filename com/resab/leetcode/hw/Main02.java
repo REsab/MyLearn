ad74e0158cf59c56f66906260f27d90b58a082d5
resab <reasb@qq.com>
2022-06-16 13:52:35 +0800

    华为机试 OD

--- com/resab/leetcode/hw/Main02.java
+++ com/resab/leetcode/hw/Main02.java
@@ -5,6 +5,15 @@ package com.resab.leetcode.hw;
 import java.util.Scanner;
 
 /**
+ *https://blog.csdn.net/weixin_47243236/article/details/123018920?ops_request_misc=&request_id=&biz_id=102&utm_term=%E5%8D%8E%E4%B8%BA%E6%9C%BA%E8%AF%95%20%E9%93%BE%E6%8E%A5%E6%8B%BC%E6%8E%A5&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-1-123018920.142^v17^pc_search_result_control_group,157^v14^new_3&spm=1018.2226.3001.4187
+ *
+ *题目描述：
+ *
+ * 给定一个url前缀和url后缀,通过,分割 需要将其连接为一个完整的url
+ * 如果前缀结尾和后缀开头都没有/，需要自动补上/连接符
+ * 如果前缀结尾和后缀开头都为/，需要自动去重
+ * 约束：不用考虑前后缀URL不合法情况
+ *
  *
  */
 public class Main02 {
