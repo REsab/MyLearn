3c0437f65b6e5bf0a68e474da61dab08cc54c470
resab <reasb@qq.com>
2022-07-25 21:27:01 +0800

    compeleteFutureTask

--- com/resab/juc01/completableFutureDemo/Demo02.java
+++ com/resab/juc01/completableFutureDemo/Demo02.java
@@ -1,5 +1,6 @@
 package com.resab.juc01.completableFutureDemo;
 
+import java.io.*;
 import java.util.concurrent.*;
 import java.util.function.Supplier;
 
@@ -32,15 +33,42 @@ public class Demo02 {
             @Override
             public Object get() {
                 System.out.println("doinng  in  executos service : " + Thread.currentThread().isDaemon());
+
+
                 return null;
             }
         }, executorService);
 
-
+//不用get方法阻塞主线程会导致任务执行中断。
         future1.get();
         future.get();
 
         executorService.shutdown();
+
+
+        // 测试
+        System.out.println("测试： ");
+        final CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println("this is lambda supplyAsync");
+            System.out.println("supplyAsync 是否为守护线程 " + Thread.currentThread().isDaemon());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("/tmp/out.txt"))))) {
+                    writer.write("this is completableFuture daemon test");
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                    System.out.println("exception find");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("this lambda is executed by forkJoinPool");
+            return "result1";
+        });
+
+        TimeUnit.SECONDS.sleep(5);
+
 
     }
 
