c06ea607566cbfdb77d579e0836d0304161dfc8d
郑跃琳 <resab@qq.com>
2023-09-05 14:09:14 +0800

    maven

--- src/test/java/com/resab/apache/MapTest.java
+++ src/test/java/com/resab/apache/MapTest.java
@@ -1,10 +1,36 @@
 package com.resab.apache;
 
 import cn.hutool.core.map.BiMap;
+import cn.hutool.core.map.TableMap;
+import org.junit.Test;
 
 import java.util.HashMap;
 
 public class MapTest {
+
+	@Test
+	public void test1() {
+		TableMap<String, Integer> tableMap = new TableMap(3);
+
+		tableMap.put("aaa", 111);
+		tableMap.put("bbb", 222);
+
+		// 111
+		tableMap.get("aaa");
+		// 222
+		tableMap.get("bbb");
+
+		// aaa
+		tableMap.getKey(111);
+		// bbb
+		tableMap.getKey(222);
+
+		// [111]
+		tableMap.getValues("aaa");
+
+		//[aaa]
+		tableMap.getKeys(111);
+	}
 
 	public static void main(String[] args) {
 
